2c344b9f46589a9ce8afe10ed058032bbdd22240
Tho Nguyen <tho.nguyen@asterist.com>
2020-02-12 16:56:43 +0900

    try to add Expenses

--- netsuitesdk/api/expenses.py
+++ netsuitesdk/api/expenses.py
@@ -8,5 +8,5 @@ class Expenses(ApiBase):
     Expenses are not directly searchable - only via as transactions
     """
     def __init__(self, ns_client):
-        ApiBase.__init__(self, ns_client=ns_client, type_name='Expenses')
+        ApiBase.__init__(self, ns_client=ns_client, type_name='expenseReport')
     
